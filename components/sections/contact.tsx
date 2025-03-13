1bd81d08058140d2cd4bb487cec3cd0c5ca1339f
Glad Ston <60247078+Iamglads@users.noreply.github.com>
2025-03-13 08:32:52 -0400

    Update contact.tsx

--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -15,7 +15,7 @@ export default function Contact() {
   });
 
   return (
-    <section className="py-24 bg-gradient-to-b from-black to-gray-900">
+    <section id="contact" className="py-24 bg-gradient-to-b from-black to-gray-900">
       <div className="container mx-auto px-4">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
